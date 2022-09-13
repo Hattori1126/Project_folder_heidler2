e82a89ed531f4816a92a454fa099a46a3d03fd41
Hattori1126 <113326735+Hattori1126@users.noreply.github.com>
2022-09-13 16:37:55 +0900

    create

--- Sync.py
+++ Sync.py
@@ -1,6 +1,16 @@
 import os
+import subprocess
 
 
+# File hostname
+# gdrive_foldername = '%s' % os.uname()[1]
+png_raspiadress = os.getcwd() + '/png'
+csv_resampled_raspiadress = os.getenv() + '/csv_resampled'
 
-print(os.uname()[1])
-print('%s' % os.uname()[1])
+# print(os.uname()[1])
+
+subprocess.run('rclone sync' + png_raspiadress + 'gdrive:' + 'Project_folder_heidler2/pnd')
+
+subprocess.run('rclone sync' + csv_resampled_raspiadress + 'gdrive:' + 'Project_folder_heidler2/csv_resampled')
+
+print('finish')
