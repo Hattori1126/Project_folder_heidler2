52e9e0ad5eac5171f8e1347212a53f4b9f44ca1f
Hattori1126 <113326735+Hattori1126@users.noreply.github.com>
2022-09-20 13:59:35 +0900

    create

--- Sync.py
+++ Sync.py
@@ -9,9 +9,13 @@ csv_resampled_raspiadress = os.getcwd() + '/csv_resampled'
 
 # print(os.uname()[1])
 
-subprocess.run(['rclone sync ' + png_raspiadress + ' gdrive:' + 'Project_folder_heidler2/png'], shell=True)
+# subprocess.run(['rclone sync ' + png_raspiadress + ' gdrive:' + 'Project_folder_heidler2/png'], shell=True)
 
-subprocess.run(['rclone sync ' + csv_resampled_raspiadress + ' gdrive:' + 'Project_folder_heidler2/csv_resampled'], shell=True)
+# subprocess.run(['rclone sync ' + csv_resampled_raspiadress + ' gdrive:' + 'Project_folder_heidler2/csv_resampled'], shell=True)
+
+subprocess.run(['rclone sync ' + png_raspiadress + ' gcs:' + 'practice_yasuilabo/hattori' + '/png'], shell=True)
+
+subprocess.run(['rclone sync ' + csv_resampled_raspiadress + ' gcs:' + 'practice_yasuilabo/hattori' + '/csv_resampled'], shell=True)
 
 
 print('finish')
